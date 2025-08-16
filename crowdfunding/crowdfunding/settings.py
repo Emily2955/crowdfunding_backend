5a81138069b0094938b6aada42586116a22ecabf
Emily Sheridan <emily.sheridan@rwwa.com.au>
2025-08-16 12:51:00 +0800

    created users

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'fundraisers.apps.FundraisersConfig',
+    'users.app.UsersConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
